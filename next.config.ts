304c8ba70440667c22f76536d4d32d46ba9306a6
Johnatan S S Paixão <johnatan.sousa@gmail.com>
2026-06-15 21:40:24 -0300

    fix: for deploy in vercel

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,6 @@ const nextConfig: NextConfig = {
   },
   compress: true,
   productionBrowserSourceMaps: false,
-  swcMinify: true,
   experimental: {
     optimizePackageImports: ["lucide-react"],
   },
